1a140d3fa9587dcabb8d62d6ec0618e24d06ba3f
Vanbq1204 <157662235+Vanbq1204@users.noreply.github.com>
2024-03-14 07:57:35 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
+        String inputFilePath = "input.txt";
+        String outputFilePath = "output.txt";
 
-
-        Bài làm viết tại đây
-
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll("Nha Trang", "Vũng Tàu");
+                writer.write(line);
+                writer.newLine();
+            }
+            System.out.println("Thay thế thành công và ghi vào file output.txt");
+            reader.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Không tìm thấy tệp input.txt");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
